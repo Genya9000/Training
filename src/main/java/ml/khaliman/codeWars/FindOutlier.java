6e3247beec701ffbc55eb880e6f2ea9d9de77f9e
Genya9000 <voliacom0034@gmail.com>
2020-01-01 23:57:08 +0200

    add task

--- src/main/java/ml/khaliman/codeWars/FindOutlier.java
+++ src/main/java/ml/khaliman/codeWars/FindOutlier.java
@@ -1,6 +1,11 @@
 package ml.khaliman.codeWars;
-public class FindOutLier {
-    static int find(int[] integers){
-        return 0;
+import java.util.Arrays;
+
+public class FindOutlier {
+   public static int find(int[] integers){
+       int[] result = null;
+       result = Arrays.stream(integers).filter(x-> (x%2==0)).count()==1? Arrays.stream(integers).filter(x-> (x%2==0))
+               .toArray() : Arrays.stream(integers).filter(x-> (x%2==1)).toArray();
+        return result[0];
 }
     }
