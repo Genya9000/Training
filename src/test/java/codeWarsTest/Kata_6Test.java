3a165c545d165ca1a95f989a931c7213c9da7639
Genya9000 <voliacom0034@gmail.com>
2020-01-02 22:12:51 +0200

    add StringTask

--- src/test/java/codeWarsTest/Kata_6Test.java
+++ src/test/java/codeWarsTest/Kata_6Test.java
@@ -136,5 +136,13 @@ public class Kata_6Test {
         assertEquals("40 + 2", Kata.expandedForm(42));
         assertEquals("70000 + 300 + 4", Kata.expandedForm(70304));
     }
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", Solution.whoLikesIt());
+        assertEquals("Peter likes this", Solution.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", Solution.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", Solution.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", Solution.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
 }
 
