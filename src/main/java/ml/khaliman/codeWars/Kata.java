5db2c25d2329c1bf8ceb758a178585a06f40a59a
Genya9000 <voliacom0034@gmail.com>
2020-01-04 23:45:15 +0200

    add PhoneNumber

--- src/main/java/ml/khaliman/codeWars/Kata.java
+++ src/main/java/ml/khaliman/codeWars/Kata.java
@@ -1,4 +1,5 @@
 package ml.khaliman.codeWars;
+import java.io.BufferedReader;
 import java.util.*;
 
 public class Kata {
@@ -7,12 +8,10 @@ public class Kata {
         int result = 0;
         String shortWord = Arrays.stream(words).min((s1, t1) -> s1.length() - t1.length()).get();
         result = shortWord.length();
-
-
         return result;
     }
-    public static String expandedForm(int num)
-    {
+
+    public static String expandedForm(int num) {
         String result = null;
         List<Integer> nums = new ArrayList<>();
         while (num != 0) {
@@ -20,17 +19,23 @@ public class Kata {
             num /= 10;
         }
         Collections.reverse(nums);
-        Integer [] res = (Integer[]) nums.toArray();
-        for (int i = 0; i <res.length ; i++) {
+        Integer[] res = (Integer[]) nums.toArray();
+        for (int i = 0; i < res.length; i++) {
             double resDouble = (double) res[i];
             resDouble *= Math.pow(10, res.length - 1 - i);
-            res[i] = (int)resDouble;
+            res[i] = (int) resDouble;
         }
-        res = (Integer[]) Arrays.stream(res).filter(x->x!=0).toArray();
-        for (int i = 0; i <res.length ; i++) {
-            result += Integer.toString((Integer) res[i])+" + ";
+        res = (Integer[]) Arrays.stream(res).filter(x -> x != 0).toArray();
+        for (int i = 0; i < res.length; i++) {
+            result += Integer.toString((Integer) res[i]) + " + ";
         }
+        return result.substring(0, result.length() - 3);
+    }
 
-        return result.substring(0, result.length()-3);
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder result = new StringBuilder("(" + numbers[0] + numbers[1] + numbers[2] +")"
+        + " " + numbers[3] + numbers[4] +numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
+
+        return result.toString();
     }
 }
