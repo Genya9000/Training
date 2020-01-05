ce780bba28cffbd2fcc9ab7d5cf7fb2a35fca872
Genya9000 <voliacom0034@gmail.com>
2020-01-05 15:43:48 +0200

    add twoSum

--- src/test/java/codeWarsTest/Kata_6Test.java
+++ src/test/java/codeWarsTest/Kata_6Test.java
@@ -153,5 +153,29 @@ public class Kata_6Test {
     public void tests() {
         assertEquals("(123) 456-7890", Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
+    @Test
+    public void basicTests()
+    {
+        doTest(new int[]{1,2,3},          new int[]{0,2});
+        doTest(new int[]{1234,5678,9012}, new int[]{1,2});
+        doTest(new int[]{2,2,3},          new int[]{0,1});
+    }
+    private void doTest(int[] numbers, int[] expected)
+    {
+        int target = numbers[expected[0]] + numbers[expected[1]];
+        int[] actual = Solution.twoSum(numbers, target);
+        if ( null == actual )
+        {
+            System.out.format("Received a null\n");
+            assertNotNull(actual);
+        }
+        if ( actual.length != 2 )
+        {
+            System.out.format("Received an array that's not of length 2\n");
+            assertTrue(false);
+        }
+        int received = numbers[actual[0]] + numbers[actual[1]];
+        assertEquals(target, received);
+    }
 }
 
